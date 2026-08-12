e3fd3dd8d71e541afad9e58d1f57b2597111426b
Nyaati <nataliaceleste520@gmail.com>
2026-08-12 18:48:01 -0400

    asd

--- usuario.py
+++ usuario.py
@@ -11,5 +11,4 @@ class Usuario:
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
-                print(f"La tarea {tarea.obtenerNombre()} está lista")
-                print(f"La tarea {tarea.obtenerNombre()} no está lista")
+                print(f"La tarea {tarea.obtenerNombre()} JAMÁS ESTARÁ LISTA")
